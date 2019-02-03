2245fc9efcf8d40a50d7daa0d0290e749d73af65
GuillaumeB <>
2019-02-02 19:08:26 -0800

    TODO: Button click

---
+++ CS485_assignment_1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private int score;
+    public Text scoreText;
+
+    public Button buttonPlay;
+    public Button buttonQuit;
+
+    void Start ()
+    {
+        speed = 3F;
+        score = 0;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce (movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag ("Pickup"))
+        {
+            other.gameObject.SetActive (false);
+            score += 1;
+            scoreText.text = "Score: " + score.ToString();
+            if (score >= 1){
+                buttonPlay.gameObject.SetActive(true);
+                buttonQuit.gameObject.SetActive(true);
+            }
+        }
+    }
+}
